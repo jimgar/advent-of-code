6c5939e94c341cffa2a107cefb2e1f75673f5993
jimgar <jimmahgee@gmail.com>
2024-12-03 21:25:47 +0000

    Alt solution for 2024 Day 3

--- 2024/03.R
+++ 2024/03.R
@@ -22,10 +22,65 @@ sum(sapply(pairs, prod))
 # Part 2
 do_input <- paste0("do()", input)
 
+# Extract the chunks bookended by do() and don't()
 pattern <- r"{do\(\).+?don't\(\)}"
 matches <- gregexpr(pattern = pattern, text = do_input)
-clean_input <- paste0(unlist(regmatches(do_input, m = matches)), collapse = "")
+cleaned_input <- paste0(
+  unlist(regmatches(do_input, m = matches)),
+  collapse = ""
+)
 
-pairs <- extract_mul_pairs(clean_input)
+pairs <- extract_mul_pairs(cleaned_input)
 
 sum(sapply(pairs, prod))
+
+
+# ---- Alternative using capture groups and `utils::strcapture` ----
+
+# I prefer my first solution (not that there's much difference!) but remember
+# coming across `utils::strcapture` a couple of times as a comment on the
+# StackOverflow post linked below. I always wanted to find a use for it, and
+# here we finally are!
+# https://stackoverflow.com/questions/952275/regex-group-capture-in-r-with-multiple-capture-groups
+
+
+# Part 1
+
+input <- readChar("2024/03-input.txt", file.info("2024/03-input.txt")$size)
+
+extract_mul_pairs <- function(input) {
+  # By using capture groups we can recycle the pattern in the function.
+  # Capture groups will be ignored by gregexpr but used by strcapture
+  pattern <- r"{mul\((\d{1,3}),(\d{1,3})\)}"
+  matches <- gregexpr(pattern = pattern, text = input)
+  muls <- unlist(regmatches(input, m = matches))
+
+  pairs <- utils::strcapture(
+    pattern = pattern,
+    x = muls,
+    proto = data.frame(left = integer(), right = integer())
+  )
+
+  return(pairs)
+}
+
+df <- extract_mul_pairs(input)
+
+sum(df$left * df$right)
+
+
+# Part 2
+
+do_input <- paste0("do()", input)
+
+# Extract the chunks bookended by do() and don't()
+pattern <- r"{do\(\).+?don't\(\)}"
+matches <- gregexpr(pattern = pattern, text = do_input)
+cleaned_input <- paste0(
+  unlist(regmatches(do_input, m = matches)),
+  collapse = ""
+)
+
+df <- extract_mul_pairs(cleaned_input)
+
+sum(df$left * df$right)
